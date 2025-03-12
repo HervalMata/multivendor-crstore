70ee85f07b188822c2325df9bc68872c68834755
Herval Mata <hervalmataamparo@gmail.com>
2025-03-12 18:35:55 -0300

    :sparkles: feat: crstore - styles and shadcn
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+
 export default function Home() {
   return (
-      <h1 className='font-bold text-pink-400'>Welcome to CrStore</h1>
+      <div className="p-5">
+        <div className="w-180 flex justify-end">
+          <ThemeToggle />
+        </div>
+        <h1 className='font-bold text-pink-400'>Welcome to CrStore</h1>
+      </div>
+
   );
 }
